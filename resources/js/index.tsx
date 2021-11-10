bb88ec09858761ef87c05c5f01ee4cbc5feb581f
Ricky <phangr28@gmail.com>
2021-11-11 00:13:40 +0700

    add chakra

--- resources/js/index.tsx
+++ resources/js/index.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
 import App from "./components/App";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 
-render(<App />, document.getElementById("app"));
+render(
+  <ChakraProvider>
+    <ColorModeScript initialColorMode="light" />
+    <App />
+  </ChakraProvider>,
+  document.getElementById("app")
+);
