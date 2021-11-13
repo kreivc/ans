be5bc171cdf59a4357b87b1c1e3c19762194e0e5
ayangd <jimmyhendrixx90@gmail.com>
2021-11-13 12:49:06 +0700

    Revert "welcome.blade.php -> index.blade.php"
    This reverts commit 8b27726f66dd39a58d067c154515a22e7e1045fb.

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('{reactRoutes}', function () {
-    return view('index');
+    return view('welcome');
 })->where('reactRoutes', '^((?!api).)*$');
