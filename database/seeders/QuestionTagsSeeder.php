0b6c149b2a7465bdd3520f4f20476e87960f79b1
Ricky <phangr28@gmail.com>
2022-01-14 16:18:21 +0700

    fix seeder

--- database/seeders/QuestionTagsSeeder.php
+++ database/seeders/QuestionTagsSeeder.php
@@ -21,8 +21,8 @@ class QuestionTagsSeeder extends Seeder
             ['question_id' => 3, 'tag_id' => 3,],
             ['question_id' => 4, 'tag_id' => 4,],
             ['question_id' => 5, 'tag_id' => 1,],
-            ['question_id' => 1, 'tag_id' => 8,],
-            ['question_id' => 5, 'tag_id' => 8,],
+            ['question_id' => 1, 'tag_id' => 5,],
+            ['question_id' => 5, 'tag_id' => 5,],
         ]);
 
     }
